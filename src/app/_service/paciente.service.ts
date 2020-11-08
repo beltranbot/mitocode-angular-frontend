847d8bfed669b152a441325c151995d268faf39c
carlos.beltran <beltranbot@gmail.com>
2020-11-08 13:45:40 -0500

    improve service by using environment file

--- src/app/_service/paciente.service.ts
+++ src/app/_service/paciente.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Paciente } from '../_model/paciente';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +10,6 @@ export class PacienteService {
   constructor(private http: HttpClient) {}
 
   listar() {
-    return this.http.get<Paciente[]>('http://localhost:8080/pacientes');
+    return this.http.get<Paciente[]>(`${environment.HOST}/pacientes`);
   }
 }
