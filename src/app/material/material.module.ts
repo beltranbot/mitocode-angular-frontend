098d0714451af9ad42c89c5338784df386264698
carlos.beltran <beltranbot@gmail.com>
2020-11-08 16:10:20 -0500

    addinng navbar and sidenavbar to the project, initial routing configuration

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [],
   imports: [],
-  exports: [MatTableModule]
+  exports: [
+    MatTableModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatDividerModule,
+
+  ]
 })
 export class MaterialModule {}
