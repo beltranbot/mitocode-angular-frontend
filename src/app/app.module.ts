e5c5d327ee2e24f61b169a872309b0975c236a66
carlos.beltran <beltranbot@gmail.com>
2020-11-08 13:07:36 -0500

    initial commit, added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PacienteComponent } from './pages/paciente/paciente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PacienteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
