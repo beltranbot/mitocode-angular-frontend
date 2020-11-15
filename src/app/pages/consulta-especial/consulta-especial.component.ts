f19f630773abb6d2a10c7756ec4cc568aba7c275
carlos.beltran <beltranbot@gmail.com>
2020-11-15 09:07:13 -0500

    added medico autocomplete in consulta-especial component

--- src/app/pages/consulta-especial/consulta-especial.component.ts
+++ src/app/pages/consulta-especial/consulta-especial.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Medico } from 'src/app/_model/medico';
 import { Paciente } from 'src/app/_model/paciente';
+import { MedicoService } from 'src/app/_service/medico.service';
 import { PacienteService } from 'src/app/_service/paciente.service';
 
 @Component({
@@ -13,26 +15,36 @@ import { PacienteService } from 'src/app/_service/paciente.service';
 export class ConsultaEspecialComponent implements OnInit {
   form: FormGroup;
   pacientes: Paciente[];
+  medicos: Medico[];
 
   // utiles para autocomplete
   myControlPaciente: FormControl = new FormControl();
   pacientesFiltrados$: Observable<Paciente[]>;
+  myControlMedico: FormControl = new FormControl();
+  medicosFiltrados$: Observable<Medico[]>;
 
-  constructor(private pacienteService: PacienteService) {}
+  constructor(
+    private pacienteService: PacienteService,
+    private medicoService: MedicoService
+  ) {}
 
   ngOnInit(): void {
     this.form = new FormGroup({
       paciente: this.myControlPaciente,
       especialidad: new FormControl(),
-      medico: new FormControl(),
+      medico: this.myControlMedico,
       fecha: new FormControl(new Date()),
       diagnostico: new FormControl(''),
       tratamiento: new FormControl(''),
     });
 
     this.listarPacientes();
+    this.listarMedicos();
     this.pacientesFiltrados$ = this.myControlPaciente.valueChanges.pipe(
       map((val) => this.filtrarPacientes(val))
+    );
+    this.medicosFiltrados$ = this.myControlMedico.valueChanges.pipe(
+      map((val) => this.filtrarMedicos(val))
     );
   }
 
@@ -60,6 +72,32 @@ export class ConsultaEspecialComponent implements OnInit {
   }
 
   mostrarPaciente(val: Paciente) {
+    return val ? `${val.nombres} ${val.apellidos}` : val;
+  }
+
+  listarMedicos() {
+    this.medicoService.listar().subscribe((data) => {
+      this.medicos = data;
+    });
+  }
+
+  filtrarMedicos(val: any) {
+    if (val != null && val.idMedico > 0) {
+      return this.medicos.filter(
+        (option) =>
+          option.nombres.toLowerCase().includes(val.nombres.toLowerCase()) ||
+          option.apellidos.toLowerCase().includes(val.apellidos.toLowerCase())
+      );
+    }
+    return this.medicos.filter(
+      (el) =>
+        el.nombres.toLowerCase().includes(val?.toLowerCase()) ||
+        el.apellidos.toLowerCase().includes(val?.toLowerCase()) ||
+        el.cmp.includes(val)
+    );
+  }
+
+  mostrarMedico(val: Medico) {
     return val ? `${val.nombres} ${val.apellidos}` : val;
   }
 
